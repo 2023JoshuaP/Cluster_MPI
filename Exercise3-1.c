e8de17c67f50e5d50f693ded1e8bdb4c5a29a1b3
2023JoshuaP <jphilco@unsa.edu.pe>
2025-09-26 13:34:17 -0500

    Testing exercise 3.1, correction MPI_Reduce.

---
+++ Exercise3-1.c
@@ -0,0 +1,103 @@
+#include <mpi.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int find_bind(float value, float min_meas, float max_meas, int bin_count) {
+    float bin_width = (max_meas - min_meas) / bin_count;
+    int bin;
+
+    bin = (int)((value - min_meas) / bin_width);
+
+    if (bin == bin_count) {
+        bin = bin_count - 1;
+    }
+
+    return bin;
+}
+
+void print_histogram(int bin_counts[], int bin_count, float min_meas, float max_meas, int data_count, int my_rank) {
+    float bin_width = (max_meas - min_meas) / bin_count;
+
+    printf("\nHistograma:\n");
+    printf("Range\t\t\tCount\n");
+    printf("------\t\t\t------");
+
+    for (int i = 0; i < bin_count; i++) {
+        float bin_max = min_meas + bin_width * (i + 1);
+        float bin_min = min_meas + bin_width * i;
+        printf("[%.2f, %.2f)\t\t%d\n", bin_min, bin_max, bin_counts[i]);
+    }
+
+    printf("\nTotal de puntos de datos: %d\n", data_count);
+}
+
+// void read_data(float data[], int data_count, float* min_meas_p, float* max_meas_p, int my_rank);
+
+int main(int argc, char* argv[]) {
+    int my_rank, comm_sz;
+    int data_count, bin_count;
+    float min_meas, max_meas;
+    float* data = NULL;
+    float* local_data;
+    int* local_bin_counts;
+    int* bin_counts = NULL;
+    int local_data_count;
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
+
+    if (my_rank == 0) {
+        printf("Ingresa el numero de puntos de datos: ");
+        fflush(stdout);
+        scanf("%d", &data_count);
+        printf("Ingresa la medida minima: ");
+        fflush(stdout);
+        scanf("%f", &min_meas);
+        printf("Ingresa la medida maxima: ");
+        fflush(stdout);
+        scanf("%f", &max_meas);
+        printf("Ingresa la cantidad de contenedores: ");
+        fflush(stdout);
+        scanf("%d", &bin_count);
+
+        data = malloc(data_count * sizeof(float));
+        bin_counts = malloc(bin_count * sizeof(int));
+
+        printf("Ingresa los datos:\n");
+        for (int i = 0; i < data_count; i++) {
+            scanf("%f", &data[i]);
+        }
+    }
+
+    MPI_Bcast(&data_count, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(&min_meas, 1, MPI_FLOAT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(&max_meas, 1, MPI_FLOAT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(&bin_count, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+    local_data = malloc(local_data_count * sizeof(float));
+    local_bin_counts = malloc(bin_count * sizeof(int));
+
+    for (int b = 0; b < bin_count; b++) {
+        local_bin_counts[b] = 0;
+    }
+
+    MPI_Scatter(data, local_data_count, MPI_FLOAT, local_data, local_data_count, MPI_FLOAT, 0, MPI_COMM_WORLD);
+
+    for (int i = 0; i < local_data_count; i++) {
+        int bin = find_bind(local_data[i], min_meas, max_meas, bin_count);
+        local_bin_counts[bin]++;
+    }
+
+    if (my_rank == 0) {
+        print_histogram(bin_counts, bin_count, min_meas, max_meas, data_count, my_rank);
+        free(data);
+        free(bin_counts);
+    }
+
+    free(local_data);
+    free(local_bin_counts);
+
+    MPI_Finalize();
+    return 0;
+}
